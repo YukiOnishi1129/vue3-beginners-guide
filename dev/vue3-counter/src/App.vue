1a5d03e2b957b7b916e35d0372ff89051a177093
yuki <yuki.onishi1129@gmail.com>
2021-07-29 17:54:36 +0900

    update: setup

--- dev/vue3-counter/src/App.vue
+++ dev/vue3-counter/src/App.vue
@@ -3,8 +3,14 @@
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
